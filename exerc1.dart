1cf6d90e11eef6cdf6554a7385f504a53bc0943d
ViniciusFeliciano <67062513+ViniciusFeliciano@users.noreply.github.com>
2020-06-18 15:05:46 -0300

    Add files via upload

---
+++ exerc1.dart
@@ -0,0 +1,8 @@
+/*Faça um programa em Dart, que exiba na tela seu nome e idade. (Não é preciso utilizar variáveis)*/
+
+import 'dart:io';
+ main ()
+ {
+    print ("Meu nome é Vinicius Marcio Feliciano");
+    print ("e tenho 15 anos de idade.");
+ }
